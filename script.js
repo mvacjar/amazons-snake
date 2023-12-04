68a934f2df2bb7dd9aff473fb40019c1283eb44b
mvacjar <mvacjar@gmail.com>
2023-12-04 09:59:02 +0100

    Fixed Dark Mode

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Declaring the variables
-const playBoard = document.querySelector('#playboard');
+const playBoard = document.querySelector("#playboard");
 let foodX, foodY;
-let snakeX = 5, snakeY = 10;
-let velocityX = 0, velocityY = 0;
+let snakeX = 5,
+  snakeY = 10;
+let velocityX = 0,
+  velocityY = 0;
 let snakeBody = [];
 let gameOver = false;
 let setIntervalId;
@@ -11,41 +13,41 @@ let setIntervalId;
 const changeFoodPosition = () => {
   foodX = Math.floor(Math.random() * 30) + 1;
   foodY = Math.floor(Math.random() * 30) + 1;
-}
+};
 
 const handleGameOver = () => {
   clearInterval(setIntervalId);
-  alert('Game Over! Press OK to Replay.');
+  alert("Game Over! Press OK to Replay.");
   location.reload();
-}
+};
 
 // Change the direction of the snake and avoid overlap
 const changeDirection = (e) => {
-  if(e.key === 'ArrowUp' && velocityY != 1) {
+  if (e.key === "ArrowUp" && velocityY != 1) {
     velocityX = 0;
     velocityY = -1;
     console.log(velocityY);
-  } else if(e.key === 'ArrowDown' && velocityY != -1) {
+  } else if (e.key === "ArrowDown" && velocityY != -1) {
     velocityX = 0;
     velocityY = 1;
-  } else if(e.key === 'ArrowLeft' && velocityX != 1) {
+  } else if (e.key === "ArrowLeft" && velocityX != 1) {
     velocityX = -1;
     velocityY = 0;
-  } else if(e.key === 'ArrowRight' && velocityX != -1) {
+  } else if (e.key === "ArrowRight" && velocityX != -1) {
     velocityX = 1;
     velocityY = 0;
   }
   initGame();
-}
+};
 
 // Initializing the game when function is called by intervals
 const initGame = () => {
-  if(gameOver) return handleGameOver();
+  if (gameOver) return handleGameOver();
 
   let placeItem = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`; // Place the food
 
   // If snake overlaps food, change food position and make snake grow +1
-  if (snakeX === foodX && snakeY === foodY) { 
+  if (snakeX === foodX && snakeY === foodY) {
     changeFoodPosition();
     snakeBody.push([foodX, foodY]);
   }
@@ -53,7 +55,7 @@ const initGame = () => {
   for (let i = snakeBody.length - 1; i > 0; i--) {
     snakeBody[i] = snakeBody[i - 1];
   }
-  
+
   // Put the first item of the array in the current position
   snakeBody[0] = [snakeX, snakeY];
 
@@ -67,31 +69,32 @@ const initGame = () => {
 
   for (let i = 0; i < snakeBody.length; i++) {
     placeItem += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
-    if(i!== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+    if (
+      i !== 0 &&
+      snakeBody[0][1] === snakeBody[i][1] &&
+      snakeBody[0][0] === snakeBody[i][0]
+    ) {
       gameOver = true;
     }
   }
 
   playBoard.innerHTML = placeItem;
-}
-
-
+};
 
 function speedUp() {
-    document.addEventListener('keydown', function(e){
-        if(e.key == ' '){
-          speed /= 2;
-          clearInterval(setIntervalId);
-          setIntervalId = setInterval(initGame, speed);
-          console.log(speed);
-        }
-        return speed
-    })
-};
+  document.addEventListener("keydown", function (e) {
+    if (e.key == " ") {
+      speed /= 2;
+      clearInterval(setIntervalId);
+      setIntervalId = setInterval(initGame, speed);
+      console.log(speed);
+    }
+    return speed;
+  });
+}
 
 let speed = 200;
 changeFoodPosition();
 speedUp();
 setIntervalId = setInterval(initGame, speed);
 document.addEventListener("keyup", changeDirection);
-
